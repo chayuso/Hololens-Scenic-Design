6188833ab4ed2763a273eb9828ab73ba7ffc5f1d
Christopher Ayuso <chayuso14@gmail.com>
2018-03-04 11:48:03 -0800

    VR Testing

---
+++ Assets/Scripts/VR/MotionControlTesting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.WSA.Input;
+using UnityEngine;
+
+public class MotionControlTesting : MonoBehaviour {
+    InteractionSourceState[] interactionSourceStates;
+    public GameObject test;
+
+	// Use this for initialization
+	void Start () {
+
+        interactionSourceStates = InteractionManager.GetCurrentReading();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        test.SetActive(false);
+        interactionSourceStates = InteractionManager.GetCurrentReading();
+        foreach (InteractionSourceState interactionSourceState in interactionSourceStates)
+        {
+            if (interactionSourceState.selectPressed)
+            {
+                test.SetActive(true);
+            }
+        }
+
+    }
+}
