31966fd3b3f5c87fa5655f4974d80623e0a491b4
kjing001 <jingke2013@gmail.com>
2018-06-02 18:49:39 -0700

    Added SetCamRotation() in Next/Pre Cam
    Also organized static and dynamic sets into "environment".
    
    Tag "environment" object with "Environment" - it is gonna be refered by camShifter.cs

--- Assets/Scripts/VR/VRCameraShifter.cs
+++ Assets/Scripts/VR/VRCameraShifter.cs
@@ -10,6 +10,7 @@ public class VRCameraShifter : NetworkBehaviour
     public GameObject[] SceneCameras;
     public GameObject CPlayer;
     public GameObject hotSpotPrefab;
+    Transform trans; // everything in the VR except the player
     public int camNum = 0;
     public bool spawnCam = false;
     // Use this for initialization
@@ -49,13 +50,41 @@ public class VRCameraShifter : NetworkBehaviour
     }
     public void SpawnCameraHotSpot()
     {
+        // spawn is the hotspot 
         var Spawn = (GameObject)Instantiate(
                 hotSpotPrefab,
                 transform.position,
                new Quaternion(1,1,1,1));
-        Spawn.transform.eulerAngles = new Vector3(0, CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.y, 0);
+
+        // save camera's orientation in the environment's reference frame
+        trans = GameObject.FindGameObjectWithTag("Environment").GetComponent<Transform>();
+        Spawn.transform.rotation = Quaternion.Inverse(trans.rotation) * Camera.main.transform.rotation;
+
+        // Spawn.transform.eulerAngles = new Vector3(0, CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.y, 0);
         BuildCamerasArray();
     }
+
+    public void SetCamRotation()
+    {
+        // saved camera's orientation in the environment's reference frame:
+        Quaternion E_q_C = SceneCameras[camNum].gameObject.transform.rotation;
+
+        // current rotation of the environment in the world refernece frame (it can be changed in AR)
+        trans = GameObject.FindGameObjectWithTag("Environment").GetComponent<Transform>();
+
+        // Camera's supposed orientation in the world refernece frame
+        Quaternion q_C = E_q_C * trans.rotation;
+
+        // Camera's parent's current transform
+        Transform cameraParentTrans = Camera.main.transform.parent;
+
+        // calculate rotation needed for "Camera Parent", in eulerAngles
+        Quaternion qRot = Quaternion.Inverse(Camera.main.transform.rotation) * q_C;
+        // rotate the cameraParent only around y
+        cameraParentTrans.Rotate(Vector3.up * qRot.eulerAngles.y);
+    }
+
+
     public void NextCam()
     {
         for (int i = 0; i < SceneCameras.Length; i++)
@@ -69,7 +98,10 @@ public class VRCameraShifter : NetworkBehaviour
             {
                 CPlayer.transform.position = SceneCameras[camNum].gameObject.transform.position;
                 //CPlayer.transform.eulerAngles = new Vector3(CPlayer.transform.eulerAngles.x, SceneCameras[camNum].gameObject.transform.eulerAngles.y, CPlayer.transform.eulerAngles.z);
-                CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles = new Vector3(CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.x, SceneCameras[camNum].gameObject.transform.eulerAngles.y, CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.z);
+                // CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles = new Vector3(CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.x, SceneCameras[camNum].gameObject.transform.eulerAngles.y, CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.z);
+                
+                SetCamRotation();
+
                 break;
             }
         }
@@ -86,8 +118,11 @@ public class VRCameraShifter : NetworkBehaviour
             if (SceneCameras[camNum].gameObject != gameObject)
             {
                 CPlayer.transform.position = SceneCameras[camNum].gameObject.transform.position;
-               // CPlayer.transform.eulerAngles = new Vector3(CPlayer.transform.eulerAngles.x, SceneCameras[camNum].gameObject.transform.eulerAngles.y, CPlayer.transform.eulerAngles.z);
-                CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles = new Vector3(CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.x, SceneCameras[camNum].gameObject.transform.eulerAngles.y, CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.z);
+                // CPlayer.transform.eulerAngles = new Vector3(CPlayer.transform.eulerAngles.x, SceneCameras[camNum].gameObject.transform.eulerAngles.y, CPlayer.transform.eulerAngles.z);
+                // CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles = new Vector3(CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.x, SceneCameras[camNum].gameObject.transform.eulerAngles.y, CPlayer.transform.Find("MixedRealityCameraParent").transform.Find("MixedRealityCamera").transform.eulerAngles.z);
+
+                SetCamRotation();
+
                 break;
             }
         }
